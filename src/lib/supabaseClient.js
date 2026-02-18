89b9acaec885ea12d32a49599f9021051489f425
Rahul0004R <rahul@hirolaits.com>
2026-02-18 14:52:44 +0530

    [Updated] CHanges in RLS

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,16 +4,13 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   {
+    realtime: {
+      transport: WebSocket
+    },
     auth: {
       persistSession: true,
       autoRefreshToken: true,
-      detectSessionInUrl: true,
-      storage: typeof window !== "undefined" ? window.localStorage : undefined
-    },
-    realtime: {
-      params: {
-        eventsPerSecond: 10
-      }
+      detectSessionInUrl: true
     }
   }
 );
